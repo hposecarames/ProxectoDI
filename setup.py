e396a3da717e5d5e725996011b22ca262662abdf
Hector <hposecarames@danielcastelao.org>
2020-06-19 04:39:31 +0200

    version beta 1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='ProxectoDI',
+   version='1.0',
+   description='Aplicación gestión',
+   author='Hector Pose Carames',
+   author_email='hposecarames@danielcastelao.org',
+   packages=['login', 'menu', 'baseDatos', 'formularios', 'facturas'],  #same as name
+   install_requires=[],
+)
